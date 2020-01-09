27659a0f924c1908dd894442c70b3deb0f5f8a06
Matthew Vance <matthew.vance@accenture.com>
2020-01-09 15:17:27 -0500

    add component generator

---
+++ plop/generators/component.js
@@ -0,0 +1,27 @@
+const templatesPath = "templates/component";
+const componentsPath = "../src/components";
+
+module.exports = {
+  description: "presentational component",
+  prompts: [
+    {
+      type: "input",
+      name: "name",
+      message: "component name"
+    }
+  ],
+  actions: [
+    {
+      type: "addMany",
+      destination: `${componentsPath}/{{ name }}`,
+      base: templatesPath,
+      templateFiles: `${templatesPath}/*.js`,
+      skipIfExists: true
+    },
+    {
+      type: "append",
+      path: `${componentsPath}/index.js`,
+      templateFile: `${templatesPath}/indexAppend.txt`
+    }
+  ]
+};
---
+++ plop/templates/component/{{ name }}.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import {{ name }} from ".";
+
+describe("{{ name }} component", () => {
+    const {/* */} = render(<{{ name }}></{{ name }}>)
+    test("", () => {
+        expect(false).toEqual(true)
+    });
+});
